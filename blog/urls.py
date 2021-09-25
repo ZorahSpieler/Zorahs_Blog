d50dfee71ffd69054d1fda20bf42bf8d0a285bf9
Zorah <AydinSpieler@web.de>
2021-09-25 18:11:03 +0200

    Modified templates edit/post

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     #name kann nachher über templatetag % post_list % aufgerufen werden
     #views.post_list greift auf def(post_list(reuqest) in views.py zu)
         path('post/<int:pk>/', views.post_detail, name='post_detail'),
+        #erzeugt ID/nummer für jeden post
+        path('post/new/', views.post_new, name='post_new'),
+        path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
 ]
