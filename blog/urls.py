81917855ded62b0dd06cc6497650b238343cd0b0
Zorah <AydinSpieler@web.de>
2021-09-25 15:51:09 +0200

    add blog-pages

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    #name kann nachher über templatetag % post_list % aufgerufen werden
+    #views.post_list greift auf def(post_list(reuqest) in views.py zu)
+]
