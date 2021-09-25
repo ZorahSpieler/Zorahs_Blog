87e4d643382ce843afa21c1a5e51f13f13d223d1
Zorah <AydinSpieler@web.de>
2021-09-25 17:43:27 +0200

    Modified templates detailpage

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     #name kann nachher über templatetag % post_list % aufgerufen werden
     #views.post_list greift auf def(post_list(reuqest) in views.py zu)
+        path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
